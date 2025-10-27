d3881b5ff07f66d89733c6030b4411d8a07755be
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2025-10-27 10:29:01 +0600

    lesson one completed

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Mazharul"
+	age := 25
+	height := 5.6
+	isProgrammer := true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Is Programmer:", isProgrammer)
+
+}
